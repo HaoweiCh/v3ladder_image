50180d51cc52637b9b1b38c03203b6339e4671d5
Heawer Cher <heawercher@gmail.com>
2019-10-16 20:23:01 +0800

    update script

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM debian:stretch-slim
-ADD v2ray /v2ray/v2ray
-WORKDIR /v2ray
-CMD ["./v2ray -version"]
+ADD v2ray /etc/v2ray/v2ray
+ADD v2ctl /etc/v2ray/v2ctl
+ADD geoip.dat /etc/v2ray/
+ADD geosite.dat /etc/v2ray/
+WORKDIR /etc/v2ray/
+CMD ["./v2ray -config config.json"]
--- download.sh
+++ download.sh
@@ -1,3 +1,4 @@
-rm v2ray-linux-amd64.zip;
+rm -f v2ray-linux-amd64.zip v2ray v2ctl *.dat;
 wget https://github.com/v2ray/v2ray-core/releases/download/v4.20.0/v2ray-linux-64.zip -O v2ray-linux-amd64.zip && \
-unzip v2ray-linux-amd64.zip "v2ray";
+unzip v2ray-linux-amd64.zip "v2ray" "v2ctl" "geoip.dat" "geosite.dat";
+chmod +w v2ray v2ctl && upx v2ray v2ctl;
