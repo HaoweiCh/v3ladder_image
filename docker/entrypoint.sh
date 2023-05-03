b63fed31443bb2cd0a5a157261da6b16793e70fc
Ch <admin@haowei.ch>
2023-05-03 11:56:49 +0800

    change app storage path.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,8 @@
-#! /bin/bash
+#!/bin/bash
 
 # Check if the image command starts with the app
 if [ "$1" = "app" ]; then
-    if [ "$PWD" = "/app" ]
-    then
+    if [ "$PWD" = "/app" ]; then
         echo "Current working directory is /app"
     else
         echo "Current working directory is not /app" && cd /app
@@ -11,30 +10,33 @@ if [ "$1" = "app" ]; then
 
     echo "Checking for required dat files..."
 
-    # Define an array of URLs for the dat files
+    # Define an associative array of URLs for the dat files with the related information
+    declare -A urls
     urls=(
-        "https://github.com/v2fly/geoip/raw/release/geoip.dat"
-        "https://github.com/v2fly/geoip/raw/release/geoip-only-cn-private.dat"
-        "https://github.com/v2fly/domain-list-community/raw/release/dlc.dat"
+        ["geoip.dat"]="https://github.com/v2fly/geoip/raw/release/geoip.dat"
+        ["geoip-only-cn-private.dat"]="https://github.com/v2fly/geoip/raw/release/geoip-only-cn-private.dat"
+        ["geosite.dat"]="https://github.com/v2fly/domain-list-community/raw/release/dlc.dat"
     )
 
     # Define a function to download a single file
     download_file() {
-        url=$1
-        filename=$(basename $url)
+        filename=$1
+        url=$2
         if [ ! -f $filename ]; then
             echo "$filename not found. Downloading..."
-            curl -s -L -o $filename $url
+            wget -q $url -O $filename
         fi
     }
 
     # Download all files concurrently using background processes
-    for url in ${urls[@]}; do
-        download_file $url &
+    for filename in "${!urls[@]}"; do
+        download_file $filename ${urls[$filename]} &
     done
     wait
 
     echo "All required dat files are present."
 fi
+
+export PATH=$PATH:/app
 
 exec $@
